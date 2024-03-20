5783f205febe89927ccffa9854fd9068cdee0fdf
Akuji <akujil26@gmail.com>
2024-03-20 19:02:10 +0100

    homework30

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,17 +1,27 @@
-import Button from "../button/Button";
-
-import { CounterProps } from "./types";
+import { useState } from 'react';
+import Button from '../button/Button';
 import { CounterWrapper, ButtonControl, Count } from "./styles";
 
-function Counter({ count, onMinus, onPlus }: CounterProps) {
+function Counter() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleMinus = () => setCount(prevCount => prevCount - 1);
+  const handlePlus = () => setCount(prevCount => prevCount + 1);
+  const handleMultiply = () => setCount(prevCount => formatNumber(prevCount * 2));
+  const handleDivide = () => setCount(prevCount => formatNumber(prevCount / 2));
+
+  const formatNumber = (num: number) => {
+    return Math.round(num * 100) / 100;
+  };
+
   return (
     <CounterWrapper>
       <ButtonControl>
-        <Button name="-" onClick={onMinus} />
-      </ButtonControl>
-      <Count>{count}</Count>
-      <ButtonControl>
-        <Button name="+" onClick={onPlus} />
+        <Button name="-" onClick={handleMinus} />
+        <Button name="Умножить" onClick={handleMultiply} />
+        <Count>{count}</Count>
+        <Button name="Разделить" onClick={handleDivide} />
+        <Button name="+" onClick={handlePlus} />
       </ButtonControl>
     </CounterWrapper>
   );
--- src/homeworks/homework29/Homework29.tsx
+++ src/homeworks/homework29/Homework29.tsx
@@ -1,16 +1,7 @@
-import { useState } from "react"
-import Counter from "../../components/counter/Counter"
+import Counter from '../../components/counter/Counter';
 
 function Homework29() {
-  const [count, setCount] = useState<number>(0)
-  const handleMinus = () => {
-    setCount(prevCount => prevCount - 1)
-  }
-  const handlePlus = () => {
-    setCount(prevCount => prevCount + 1)
-  }
-
-  return <Counter count={count} onMinus={handleMinus} onPlus={handlePlus} />
+  return <Counter />;
 }
 
-export default Homework29
+export default Homework29;
