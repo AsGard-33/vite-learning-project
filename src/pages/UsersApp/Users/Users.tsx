581bd2fc426fadc6bdf18584bef5e5e37d3e7c60
Akuji <akujil26@gmail.com>
2024-03-24 14:07:56 +0100

    homework32

--- src/pages/UsersApp/Users/Users.tsx
+++ src/pages/UsersApp/Users/Users.tsx
@@ -1,7 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { usersSliceActions, usersSliceSelectors } from "store/redux/users/usersSlice";
+import Button from 'components/button/Button';
 import { UsersPageWrapper, UserCard, Paragraph } from "./styles"
 
 function Users() {
-  return <UsersPageWrapper></UsersPageWrapper>
+  const dispatch = useDispatch();
+  const users = useSelector(usersSliceSelectors.users);
+
+  const handleDeleteUser = (userId: string) => {
+    dispatch(usersSliceActions.removeUser(userId));
+  };
+
+  const handleDeleteAllUsers = () => {
+    dispatch(usersSliceActions.removeAllUsers());
+  };
+
+  return <UsersPageWrapper>
+    {users.length > 0 && <Button name='Delete all users' onClick={handleDeleteAllUsers}></Button>}
+      {users.map((user) => (
+        <UserCard key={user.id}>
+          <Paragraph>Name: {user.firstlastName}</Paragraph>
+          <Paragraph>Age: {user.age}</Paragraph>
+          <Paragraph>Job Title: {user.jobTitle}</Paragraph>
+          <Button name='Delete' onClick={() => handleDeleteUser(user.id)}></Button>
+        </UserCard>
+      ))}
+  </UsersPageWrapper>
 }
 
 export default Users
